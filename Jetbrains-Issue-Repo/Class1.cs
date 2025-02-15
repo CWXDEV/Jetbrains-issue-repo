765acd2c0fab6635bb08e2d60b77d82e91189582
CWX <cwxdev@outlook.com>
2025-02-15 01:07:17 +0000

    Hopefully that helps

---
+++ Jetbrains-Issue-Repo/Class1.cs
@@ -0,0 +1,14 @@
+﻿using EFT;
+using EFT.Weather;
+
+namespace Jetbrains_Issue_Repo;
+
+public class Class1
+{
+    public static void Method1()
+    {
+        var weatherController = new WeatherController();
+
+        weatherController.WeatherDebug.Fog = 0;
+    }
+}
